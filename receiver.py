af4102885dc76a48d051136ff71b7eff22893d25
Mast3rwaf1z <thomasmollerjensen@live.dk>
2021-11-12 17:59:00 +0000

    added some fec stuff

--- receiver.py
+++ receiver.py
@@ -1,17 +1,34 @@
 import bluebox as bb
+import fec
+import codecs
+import binascii
+
 
 rx:bb.Bluebox
+fechandler = fec.PacketHandler(key="test")
 
-def rx_init(serial="dead0024", freq=434500000) -> bb.Bluebox:
+
+def rx_init(serial="dead0024", freq=434500000, mod=1, timeout=10000, bitrate=9200) -> bb.Bluebox:
     rx = bb.Bluebox(serial=serial)
     rx.rx_mode()
     rx.set_frequency(freq)
+    rx.set_modindex(mod)
+    rx.timeout = timeout
+    rx.set_bitrate(bitrate)
     return rx
 
-def receive(self:bb.Bluebox) -> str:
-    return self.receive()
+def receive(self:bb.Bluebox):
+    data = None
+    while data is None:
+        data,rssi,freq = self.receive()
+    #codecs.decode(data[0],"0x00f2448e01")
+    packet,_,_ = fechandler.deframe(data)
+    return packet
+
 
 if __name__ == "__main__":
-    rx = rx_init()
+    rx = rx_init(bitrate=2400)
     while True:
-        print(receive(rx))
+        packet = receive(rx)
+        if packet is not None:
+            print(bytes.decode(binascii.unhexlify(packet), "utf-8"))
--- transmitter.py
+++ transmitter.py
@@ -1,19 +1,23 @@
+import codecs
 import bluebox as bb
+import fec
+import binascii
 
-tx:bb.Bluebox
-
-def tx_init(serial="00000003", freq=434500000) -> bb.Bluebox:
+def tx_init(serial="00000003", freq=434500000, power=8, mod=1, bitrate=9200) -> bb.Bluebox:
     tx = bb.Bluebox(serial=serial) #get serial key from bbctl list
     tx.tx_mode()
     tx.set_frequency(freq)
-    tx.set_power(0)
+    tx.set_power(power)
+    tx.set_modindex(mod)
+    tx.set_bitrate(bitrate)
     return tx
 
-def tx_transmit(tx:bb.Bluebox, packet:str = "ping") -> tuple:
-    tx.transmit(bytes(packet, "utf-8"))
+def transmit(tx:bb.Bluebox, packet:str = "ping") -> tuple:
+    fecc = fec.PacketHandler(key="test")
+    data = fecc.frame(binascii.hexlify(bytes(packet, "utf-8")))
+    tx.transmit(data)
     return packet
 
 if __name__ == "__main__":
-    tx = tx_init()
-    while True:
-        tx_transmit(tx, input("input: "))
+    tx = tx_init(bitrate=2400, power=0)
+    transmit(tx, input("input:"))
