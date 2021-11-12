6abc932fbaf1a81d30bec673e19793a1383208cb
Mast3rwaf1z <thomasmollerjensen@live.dk>
2021-11-12 18:39:05 +0000

    fixed some stuff, got big bb to work

--- transmitter.py
+++ transmitter.py
@@ -1,7 +1,6 @@
 import codecs
 import bluebox as bb
 import fec
-<<<<<<< HEAD
 import binascii
 
 def tx_init(serial="00000003", freq=434500000, power=8, mod=1, bitrate=9200) -> bb.Bluebox:
@@ -16,29 +15,9 @@ def tx_init(serial="00000003", freq=434500000, power=8, mod=1, bitrate=9200) -> 
 def transmit(tx:bb.Bluebox, packet:str = "ping") -> tuple:
     fecc = fec.PacketHandler(key="test")
     data = fecc.frame(binascii.hexlify(bytes(packet, "utf-8")))
-=======
-
-
-def tx_init(serial="00000003", freq=434500000) -> bb.Bluebox:
-    tx = bb.Bluebox(serial=serial) #get serial key from bbctl list
-    tx.tx_mode()
-    tx.set_frequency(freq)
-    tx.set_power(4)
-    tx.set_modindex(1)
-    return tx
-
-def transmit(tx:bb.Bluebox, packet:str = "ping") -> tuple:
-    fecc = fec.PacketHandler(key="nickersej")
-    data = fecc.frame(codecs.encode("0x00f2448e01"))
->>>>>>> c4a4ed47811072ac550f8860fdcf6aaf0833a4d4
     tx.transmit(data)
     return packet
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    tx = tx_init(bitrate=2400, power=0)
+    tx = tx_init(bitrate=2400, power=8)
     transmit(tx, input("input:"))
-=======
-    tx = tx_init()
-    transmit(tx)
->>>>>>> c4a4ed47811072ac550f8860fdcf6aaf0833a4d4
