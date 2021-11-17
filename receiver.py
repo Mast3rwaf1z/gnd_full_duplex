c4a4ed47811072ac550f8860fdcf6aaf0833a4d4
Mast3rwaf1z <thomasmollerjensen@live.dk>
2021-11-17 17:36:39 +0100

    added nick stuff

--- receiver.py
+++ receiver.py
@@ -2,13 +2,14 @@ import bluebox as bb
 
 rx:bb.Bluebox
 
-def rx_init(serial="dead0024", freq=434500000) -> bb.Bluebox:
+def rx_init(serial="00000002", freq=434500000) -> bb.Bluebox:
     rx = bb.Bluebox(serial=serial)
     rx.rx_mode()
     rx.set_frequency(freq)
+    rx.set_modindex(1)
     return rx
 
-def receive(self:bb.Bluebox) -> str:
+def receive(self:bb.Bluebox):
     return self.receive()
 
 if __name__ == "__main__":
--- transmitter.py
+++ transmitter.py
@@ -1,19 +1,22 @@
+import codecs
 import bluebox as bb
+import fec
 
-tx:bb.Bluebox
 
 def tx_init(serial="00000003", freq=434500000) -> bb.Bluebox:
     tx = bb.Bluebox(serial=serial) #get serial key from bbctl list
     tx.tx_mode()
     tx.set_frequency(freq)
-    tx.set_power(0)
+    tx.set_power(4)
+    tx.set_modindex(1)
     return tx
 
-def tx_transmit(tx:bb.Bluebox, packet:str = "ping") -> tuple:
-    tx.transmit(bytes(packet, "utf-8"))
+def transmit(tx:bb.Bluebox, packet:str = "ping") -> tuple:
+    fecc = fec.PacketHandler(key="nickersej")
+    data = fecc.frame(codecs.encode("0x00f2448e01"))
+    tx.transmit(data)
     return packet
 
 if __name__ == "__main__":
     tx = tx_init()
-    while True:
-        tx_transmit(tx, input("input: "))
+    transmit(tx)
