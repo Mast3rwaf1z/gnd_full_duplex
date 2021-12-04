f3558b2597e95512e2ca549956867b5d49c59631
Mast3rwaf1z <thomasmollerjensen@live.dk>
2021-12-04 18:01:34 +0100

    added a bit of error handling

--- GUI.py
+++ GUI.py
@@ -3,16 +3,30 @@ import tkinter as tk
 import full_duplex as fd
 
 BBH:fd.dualBB_handler = None
-
 def exec_fd():
-    BBH = fd.dualBB_handler()
-    txThread = fd.tx_thread(BBH)
-    rxThread = fd.rx_thread(BBH)
+    try:
+        BBH = fd.dualBB_handler()
+        txThread = fd.tx_thread(BBH)
+        rxThread = fd.rx_thread(BBH)
+        textbox.config(text="successfully initialized full duplex", fg="green")
+    except Exception as ex:
+        textbox.config(text="failed to initialize full duplex, is two blueboxes plugged in and initialized correctly?", fg="red")
+        print(e)
 
 def queue():
     if BBH is not None:
         BBH.tq.put(e.get())
         print(BBH.tq.size)
+        textbox.config(text="successfully queued item", fg="red")
+    else:
+        textbox.config(text="failed to queue item, is BBH initialized?", fg="red")
+
+def get_queue():
+    if BBH is not None:
+        print(BBH.tq.items)
+        textbox.config(text="successfully got queue", fg="green")
+    else:
+        textbox.config(text="failed to get queue, is BBH initialized?", fg="red")
 
 root = tk.Tk()
 frame = tk.Frame(root)
@@ -24,12 +38,18 @@ button = tk.Button(frame,
                    command=quit)
 button.pack(side=tk.LEFT)
 exec_button = tk.Button(frame,
-                   text="exec full-duplex",
+                   text="init full-duplex",
                    command=exec_fd)
 exec_button.pack(side=tk.LEFT)
 
 queue_button = tk.Button(frame, text="queue", command=queue)
 queue_button.pack(side=tk.LEFT)
+
+get_queue_button = tk.Button(frame, text="Get Queue", command=get_queue)
+get_queue_button.pack(side=tk.BOTTOM)
+
+textbox = tk.Label(root, fg="green")
+textbox.pack(side=tk.BOTTOM)
 
 e = tk.Entry(root)
 e.pack(side=tk.BOTTOM)
