406f4af9cb5c9d408468cb1a677a719f3504a3b8
Mast3r_waf1z <32744171+Mast3rwaf1z@users.noreply.github.com>
2022-01-25 01:13:05 +0100

    added override methods to queue
    added some basic functionality like representatives and len methods to the queue object such that it will be easier to work with in the future

--- modules/data_structures.py
+++ modules/data_structures.py
@@ -1,7 +1,14 @@
 class queue():
+    #some definitions native to python, will enable functionality as to get a representative when calling
+    #obj instead of obj.size and functionality to len
     def __init__(self):
         self.items = []
         self.size = 0
+    def __len__(self):
+        return self.size
+    def __repr__(self):
+        return str(self.items)
+    #data structure design by the book
     def enqueue(self, item):
         self.items.append(item)
         self.size += 1
@@ -13,3 +20,13 @@ class queue():
             return None
     def clear(self):
         self.__init__()
+
+
+
+if __name__ == "__main__":
+    Q = queue()
+    for i in range(10):
+        Q.enqueue(i)
+    print(str(len(Q))+str(Q))
+    Q.dequeue()
+    print(str(len(Q))+str(Q))
